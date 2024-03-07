780cd9caba2b074103422a45fcd4c5e1be4d7382
marcelinasen <marcelina.sentyrz@gmail.com>
2024-03-07 14:04:49 +0100

    commit-3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+let timer = null;
 
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+buttonStart.addEventListener('click', function() {
+    this.disabled = true;
+    buttonStop.disabled = false;
+    changeBackgroundColor();
+    timer = setInterval(changeBackgroundColor, 1000);
+});
+
+buttonStop.addEventListener('click', function() {
+    this.disabled = true;
+    buttonStart.disabled = false;
+    clearInterval(timer);
+}); 
